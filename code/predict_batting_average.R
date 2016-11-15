383655dadf4df2217427e98227bbf2212e4c07c7
liberaliscomputing <liberalis.computing@gmail.com>
2016-11-15 00:22:11 -0500

    initialize code for answer #2

---
+++ code/predict_batting_average.R
@@ -0,0 +1,10 @@
+# Candidate’s Choice Question 
+#   for the Senior Quantitative Analyst Position at Philadelphia Phillies
+# Submission: November 15, 2016
+# Candidate: Meen Chul Kim
+
+# Read in data
+batting <- read.csv('data/batting.csv')
+
+# Histogram to check the distribution of data
+hist(batting$MarApr_AVG)
